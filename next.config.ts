294dd719a96c94dcaaa9a90519105a56042130d6
facun3625 <59091597+facun3625@users.noreply.github.com>
2026-06-02 17:48:58 -0300

    chore: actualizar next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,7 @@ import path from "path";
 const nextConfig: NextConfig = {
   outputFileTracingRoot: path.join(__dirname),
   images: {
-    localPatterns: [
-      { pathname: "/uploads/**", search: "" },
-      { pathname: "/images/**", search: "" },
-    ],
+    unoptimized: true,
   },
 };
 
